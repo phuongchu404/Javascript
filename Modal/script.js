4f0d7ef9e56a1be8e7062499763217fa3a7829e3
Phuong-Chu <phuongchu404@gmail.com>
2023-11-28 14:13:40 +0700

    update file script.js

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,23 @@
 "use strict";
+const openModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+console.log(openModal);
+console.log(modal);
+console.log(overlay);
+console.log(modal.classList);
+console.log(overlay.classList);
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const close = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+openModal.forEach((btn) => {
+  btn.addEventListener("click", showModal);
+});
+closeModal.addEventListener("click", close);
+overlay.addEventListener("click", close);

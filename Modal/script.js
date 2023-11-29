ea3fd96850b66dd0a6e883c0b3a3b21383d72a57
Phuong-Chu <phuongchu404@gmail.com>
2023-11-29 13:46:58 +0700

    Revert "Merge branch 'main' into develop"
    This reverts commit d25fff6c03cf04c767f2215ce36edba3e560072c, reversing
    changes made to 68d193be76c3e19537f16304bd006e88c9863b98.

--- Modal/script.js
+++ Modal/script.js
@@ -3,16 +3,17 @@ const openModal = document.querySelectorAll(".show-modal");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
-console.log(openModal);
-console.log(modal);
-console.log(overlay);
-console.log(modal.classList);
-console.log(overlay.classList);
+// console.log(openModal);
+// console.log(modal);
+// console.log(overlay);
+// console.log(modal.classList);
+// console.log(overlay.classList);
 const showModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 const close = function () {
+  console.log("close");
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
@@ -21,3 +22,15 @@ openModal.forEach((btn) => {
 });
 closeModal.addEventListener("click", close);
 overlay.addEventListener("click", close);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e);
+  console.log(modal.classList);
+  if (e.key === "Escape") {
+    console.log("escape key");
+    if (!modal.classList.contains("hidden")) {
+      close();
+    }
+    console.log(modal.classList);
+  }
+});

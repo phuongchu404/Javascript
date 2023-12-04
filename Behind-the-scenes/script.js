164358e49747ff5f3683d12cdf1494ea4b8f1dbc
Phuong-Chu <phuongchu404@gmail.com>
2023-12-04 10:30:44 +0700

    change file in folder behind in scene

---
+++ Behind-the-scenes/script.js
@@ -0,0 +1,27 @@
+"use strict";
+const calAge = (birthday) => {
+  const age = 2024 - birthday;
+  function printAge() {
+    console.log(`my name is ${name}, my age is ${age}`);
+    if (age > 20) {
+      var address = "VietNam";
+      let str = "Too old";
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(address);
+    //console.log(str);
+    //console.log(add(2, 3));
+  }
+  printAge();
+  return age;
+};
+const name = "phuong";
+calAge(2000);
+console.log(x);
+var x = 9;
+console.log(add(2, 3));
+function add(a, b) {
+  return a + b;
+}
